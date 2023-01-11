868079730feadee810904f2a54caae2530b826cd
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-01-11 18:01:44 -0300

    client + project relationship

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -21,10 +21,15 @@ const ProjectType = new GraphQLObjectType({
   name: 'Project',
   fields: () => ({
     id: { type: GraphQLID },
-    clientId: { type: GraphQLID },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
     status: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return clients.find((client) => client.id === parent.clientId);
+      },
+    },
   }),
 });
 
