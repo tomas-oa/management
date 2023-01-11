c185ecf9e8823e5febfd4b7853340a8888294cc1
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-01-11 20:10:21 -0300

    clients + projects querys to retrieve data from db with mongoose

--- server/models/Project.js
+++ server/models/Project.js
@@ -17,4 +17,4 @@ const ProjectSchema = new mongoose.Schema({
   },
 });
 
-export default mongoose.model('Project', ProjectSchema);
+module.exports = mongoose.model('Project', ProjectSchema);
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,6 +6,8 @@ const {
   GraphQLSchema,
   GraphQLList,
 } = require('graphql');
+const Project = require('../models/Project');
+const Client = require('../models/Client');
 
 const ClientType = new GraphQLObjectType({
   name: 'Client',
@@ -27,7 +29,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find((client) => client.id === parent.clientId);
+        return Client.findById(parent.clientId);
       },
     },
   }),
@@ -39,7 +41,7 @@ const RootQuery = new GraphQLObjectType({
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients;
+        return Client.find({});
       },
     },
 
@@ -48,7 +50,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
 
       resolve(parent, args) {
-        return clients.find((client) => client.id === args.id);
+        return Client.findById(args.id);
       },
     },
 
@@ -56,7 +58,7 @@ const RootQuery = new GraphQLObjectType({
       type: new GraphQLList(ProjectType),
 
       resolve(parent, args) {
-        return projects;
+        return Project.find({});
       },
     },
 
@@ -65,7 +67,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
 
       resolve(parent, args) {
-        return projects.find((project) => project.id === args.id);
+        return Project.findById(args.id);
       },
     },
   },
