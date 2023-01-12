66c0359fc5c5bf79780c93bb2eab7267d3b003d1
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-01-12 00:21:27 -0300

    form functionality to create clients

--- client/src/components/AddClient.jsx
+++ client/src/components/AddClient.jsx
@@ -1,11 +1,37 @@
 import { useState } from 'react';
 import { FaUser } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
+import { ADD_CLIENT } from '../mutations/client';
+import { GET_CLIENTS } from '../querys/client';
 
 function AddClient() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
+
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: { name, email, phone },
+    update: (cache, { data: { addClient } }) => {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: [...clients, addClient] },
+      });
+    },
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (name === '' || email === '' || phone === '') {
+      return alert('All fields are required');
+    }
+
+    addClient();
+    setName('');
+    setEmail('');
+    setPhone('');
+  };
 
   return (
     <>
@@ -42,7 +68,7 @@ function AddClient() {
               ></button>
             </div>
             <div className="modal-body">
-              <form>
+              <form onSubmit={onSubmit}>
                 <div className="mb-3">
                   <label className="form-label">Name</label>
                   <input
